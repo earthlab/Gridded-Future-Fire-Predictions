20d245c0f8c52981876f1fb1991c0fcc4e4f39aa
Erick Verleye <erve3705@colorado.edu>
2024-03-12 16:56:42 -0600

    Parallelizing fit-count-zinb-nuts.R

--- R/scripts/fit-count-zinb-nuts.R
+++ R/scripts/fit-count-zinb-nuts.R
@@ -1,5 +1,6 @@
 library(readr)
 library(rstan)
+library(parallel)
 
 library(here)
 
@@ -12,22 +13,25 @@ subregions = c("L5", "L6_1", "L6_2", "L7", "L8_1", "L8_2", "L8_3", "L9_1", "L9_2
 fit_count <- function(model_name) { 
     stan_dir = file.path(processed_data_dir, "stan", model_name)
 
-    for (subregion in subregions) {
+    # Determine the number of cores to utilize
+    no_cores <- detectCores() - 1  # Leave one core for the main process
+
+    # Parallelize using mclapply (similar to lapply but in parallel)
+    mclapply(subregions, function(subregion) {
         zi_d <- read_rds(file.path(stan_dir, paste0('zi_d', subregion, '.rds')))
 
         zinb_init <- stan_model(file.path(raw_data_dir, 'counts-zinb.stan'))
         zinb_full_fit <- sampling(zinb_init,
-                                data = zi_d,
-                                init_r = 0.01,
-                                iter = 1000, 
-                                pars =  c('mu_full','count_pred'),
-                                cores = 16)
+                                  data = zi_d,
+                                  init_r = 0.01,
+                                  iter = 1000, 
+                                  pars =  c('mu_full','count_pred'),
+                                  cores = 16)  
 
-        post <- rstan::extract(zinb_full_fit,pars = 'count_pred')
+        post <- rstan::extract(zinb_full_fit, pars = 'count_pred')
 
         write_rds(post, path = file.path(stan_dir, paste0('post', subregion, '.rds')))
-    }
-
+    }, mc.cores = no_cores) 
 }
 
 models <- c("CanESM2", "CNRM", "CSIRO", "HadGEM2-CC", "HadGEM2-ES", "IPSL", "MIROC5", "MRI")
